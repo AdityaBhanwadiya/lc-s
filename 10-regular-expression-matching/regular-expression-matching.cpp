8e9b235ce43453924251a863223ff39d070a6a11
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-02-08 10:03:07 -0500

    Time: 3 ms (49.34%) | Memory: 9.1 MB (51.67%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    unordered_map<string, bool> yaad; // Memoization map
+    bool isMatch(string s, string p) {
+        int u = s.size(), v = p.size();
+        vector<vector<bool>> patra(u + 1, vector<bool>(v + 1, false));
 
-    bool madad(string s, string p, int i, int j) {
-        string key = to_string(i) + "," + to_string(j);
-        if (yaad.count(key)) return yaad[key]; // Check memo
+        patra[0][0] = true;
 
-        if (j == p.size()) return i == s.size(); // Base case
-        
-        bool match = (i < s.size() && (s[i] == p[j] || p[j] == '.'));
-
-        if (j + 1 < p.size() && p[j + 1] == '*') {
-            return yaad[key] = madad(s, p, i, j + 2) ||  // Ignore `p[j]*`
-                               (match && madad(s, p, i + 1, j)); // Use `p[j]`
+        // Handle '*' in pattern
+        for (int j = 1; j <= v; j++) {
+            if (p[j - 1] == '*') {
+                patra[0][j] = patra[0][j - 2]; // '*' removes previous char
+            }
         }
 
-        return yaad[key] = match && madad(s, p, i + 1, j + 1);
-    }
+        for (int i = 1; i <= u; i++) {
+            for (int j = 1; j <= v; j++) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '.') {
+                    patra[i][j] = patra[i - 1][j - 1]; // Direct match or '.'
+                } 
+                else if (p[j - 1] == '*') {
+                    patra[i][j] = patra[i][j - 2];  // '*' removes previous char
+                    if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
+                        patra[i][j] = patra[i][j] || patra[i - 1][j];  // '*' repeats
+                    }
+                }
+            }
+        }
 
-    bool isMatch(string s, string p) {
-        return madad(s, p, 0, 0);
+        return patra[u][v];
     }
 };
