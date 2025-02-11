ed2bc118ff52211b07eaf464a65bf1a10618ca70
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-02-11 16:31:21 -0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (60.62%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,38 +1,70 @@
-class Solution {
+class DisjointSets {
+    vector<int> rank, size, parent;
+
 public:
-    void dfs(int node, vector<vector<int>>& adjList, vector<int>& vis) {
-        vis[node] = 1;
-        for (auto neighbor : adjList[node]) {
-            if (!vis[neighbor]) {
-                dfs(neighbor, adjList, vis);
-            }
+    DisjointSets(int n) {
+        rank.resize(n+1, 0);
+        size.resize(n+1, 1);
+        parent.resize(n+1);
+
+        for(int i=1;i<=n;i++) {
+            parent[i] = i;
         }
     }
 
+    int findParent(int node) {
+        if(node == parent[node])
+            return node;
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void findUnionByRank (int u, int v) {
+        int ul_pu = findParent(u);
+        int ul_pv = findParent(v);
+
+        if(ul_pu == ul_pv)  return;
+
+        if(rank[ul_pu] < rank[ul_pv]) {
+            parent[ul_pu] = ul_pv;
+        }else if(rank[ul_pv] < rank[ul_pu]){
+            parent[ul_pv] = ul_pu;
+        }else{
+            parent[ul_pv] = ul_pu;
+            rank[ul_pu]++;
+        }
+    }
+
+    void findUnionBySize(int u, int v) {
+        int ul_pu = findParent(u);
+        int ul_pv = findParent(v);
+
+        if(ul_pu == ul_pv)  return;
+        if(size[ul_pu] < size[ul_pv]) {
+            parent[ul_pu] = ul_pv;
+            size[ul_pv] += size[ul_pu];
+        }else{
+            parent[ul_pv] = ul_pu;
+            size[ul_pu] += size[ul_pv];
+        }
+    }
+};
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<vector<int>> adjList(n); // Initialize adjacency list
+        DisjointSets ds(n);
 
-        // Create adjacency list
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(isConnected[i][j] == 1 && i != j) { // Avoid self-loops
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
-                }
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if (isConnected[i][j] == 1)
+                    ds.findUnionBySize(i, j);
             }
         }
 
-        vector<int> vis(n, 0);
         int count = 0;
-
-        // Count connected components
-        for(int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                count++;
-                dfs(i, adjList, vis);
-            }
+        for(int i=0;i<n;i++){
+            if(ds.findParent(i) == i)   count++;
         }
-        return count;
+    return count;
     }
 };
