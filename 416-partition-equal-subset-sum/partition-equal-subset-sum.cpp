e044f8f17ee2f5ed1dd9756043ad0bec86e6b56c
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-02-19 12:16:59 -0500

    Time: 131 ms (47.50%) | Memory: 13.4 MB (87.02%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -21,27 +21,36 @@ private:
 
     bool isSubsetSum(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(k + 1, 0));
+        // vector<vector<bool>> dp(n + 1, vector<bool>(k + 1, 0));
+        vector<bool> prev(k+1, 0), curr(k+1, 0);
 
-        for(int i=0;i<n;i++)    dp[i][0]=0;
+        // for(int i=0;i<n;i++)    dp[i][0]=0;
 
-        if (arr[0] <= k) {
-            dp[0][arr[0]] = true;
+        // if (arr[0] <= k) {
+        //     dp[0][arr[0]] = true;
+        // }
+
+        if (n > 0) {
+            prev[0] = curr[0] = true;
+            if (arr[0] <= k) prev[arr[0]] = true;
         }
+
 
         for(int ind=1;ind<n;ind++) {
             for(int target = 1;target<=k;target++){
-                bool notTaken = dp[ind - 1][target];
+                bool notTaken = prev[target];
 
                 bool taken = false;
                 if (arr[ind] <= target)
-                    taken = dp[ind - 1][target - arr[ind]];
+                    taken = prev[target - arr[ind]];
 
-                dp[ind][target] = notTaken || taken;
+                curr[target] = notTaken || taken;
             }
+            prev = curr;
         }
+        
 
-        return dp[n - 1][k];
+        return prev[k];
     }
 public:
     bool canPartition(vector<int>& nums) {
