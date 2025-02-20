2614c3c08d56ce228b477c07e4ce7d362a4daa80
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-02-20 13:21:41 -0500

    Time: 53 ms (21.12%) | Memory: 48.9 MB (34.21%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,28 +2,42 @@ class Solution {
 private:
     int func (int i, int amount, vector<int>& coins, vector<vector<int>>& dp) {
         // base
-        if(i == 0){
-            if(amount % coins[0] == 0)  return amount/coins[0];
-            return 1e9;
+        // if(i == 0){
+        //     if(amount % coins[0] == 0)  return amount/coins[0];
+        //     return 1e9;
+        // }
+
+        for(int i = 1;i<=amount;i++){
+            if(i%coins[0] == 0)    
+                dp[0][i] = i/coins[0];
+            else
+                dp[0][i] = 1e9;
         }
 
-        if(dp[i][amount] != -1) 
-            return dp[i][amount];
+        // if(dp[i][amount] != -1) 
+        //     return dp[i][amount];
 
         //take
-        int take = INT_MAX;
-        if(amount >= coins[i])
-            take = 1 + func (i, amount - coins[i], coins, dp);
-        //not take
-        int notTake = 0 + func(i-1, amount, coins, dp);
 
-        return dp[i][amount] = min(take, notTake);
+        for(int i=1;i<coins.size();i++){
+            for(int k = 1;k<=amount;k++) {
+                int take = INT_MAX;
+                if(k >= coins[i])
+                    take = 1 + dp[i][k - coins[i]];
+                int notTake = 0 + dp[i-1][k];
+
+                dp[i][k] = min(take, notTake);
+            }
+        }
+        
+
+        return dp[i][amount];
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
 
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
         int ans = func (n-1, amount, coins, dp);
         if(ans >= 1e9)
             return -1;
