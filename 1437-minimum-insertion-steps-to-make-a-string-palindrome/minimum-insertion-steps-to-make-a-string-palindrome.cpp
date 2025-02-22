7ba48c6bdcc031196d5313c95478699280e4a974
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-02-22 14:59:56 -0500

    Time: 23 ms (78.62%) | Memory: 9.9 MB (85.94%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,48 @@
+class Solution {
+private:
+    int func (int i, int j, string& s, string& t, vector<int>& prev, vector<int>& curr) {
+        //base
+        // if(i < 0 || j < 0){
+        //     return 0;
+        // }
+
+        // for(int i=0;i<=s.length();i++){
+        //     dp[i][0] = 0;
+        // }
+
+        for(int i=0;i<=t.length();i++){
+            prev[i] = 0;
+        }
+
+        // if(dp[i][j] != -1)  return dp[i][j];
+
+        for(int i=1;i<=s.length();i++) {
+            for(int j=1;j<=t.length();j++){
+                if(s[i-1] == t[j-1]){
+                    curr[j] = 1 + prev[j-1];
+                }else{
+                    curr[j] = max(prev[j], curr[j-1]);
+                }
+            }
+            prev= curr;
+        }
+        return prev[t.length()];
+    }
+    int longestCommonSubsequence(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        vector<int>prev(m+1, 0), curr(m+1, 0);
+        return func (n, m, s, t, prev, curr);
+    }
+    int longestPalindromeSubseq(string s) {
+        string t = s;
+        reverse(t.begin(), t.end());
+        return longestCommonSubsequence(s, t);
+    }
+public:
+    int minInsertions(string s) {
+        int len = longestPalindromeSubseq(s);
+        return s.length() - len;
+    }
+};
