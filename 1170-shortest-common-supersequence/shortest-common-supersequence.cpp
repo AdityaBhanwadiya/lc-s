a7a6f2603bf9100d824f0ef5f053428fb3624206
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-02-23 09:24:18 -0500

    Time: 26 ms (51.10%) | Memory: 26.7 MB (70.31%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -38,15 +38,13 @@ public:
         int i = n;
         int j = m;
 
-        int index = len - 1;
         string ans = "";
 
         while (i > 0 && j > 0) {
             if (s1[i - 1] == s2[j - 1]) {
-            ans += s1[i-1];
-            index--;
-            i--;
-            j--;
+                ans += s1[i-1];
+                i--;
+                j--;
             } else if (dp[i - 1][j] > dp[i][j - 1]) {
                 ans += s1[i-1];
                 i--;
