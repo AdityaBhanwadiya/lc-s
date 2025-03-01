f1d8b5d40fa462ad8845ba2409c80c8232f168f7
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-03-01 09:55:52 -0500

    Time: 0 ms (100.00%) | Memory: 13.1 MB (87.59%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -8,24 +8,16 @@ public:
                 nums[i+1] = 0;
             }
         }   
-        vector<int> nos;
-        int zeros = 0;
-
-        for(int i=0;i<n;i++){
-            if(nums[i] == 0)
-                zeros++;
-            else
-                nos.push_back(nums[i]);
+        int index = 0; 
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
         }
-
-        nums.clear();
-
-        for(int i=0;i<nos.size();i++)
-            nums.push_back(nos[i]);
         
-        for(int i=0;i<zeros;i++)
-            nums.push_back(0);
-
+        while(index < n) {
+            nums[index++] = 0;
+        }
 
         return nums;
     }
