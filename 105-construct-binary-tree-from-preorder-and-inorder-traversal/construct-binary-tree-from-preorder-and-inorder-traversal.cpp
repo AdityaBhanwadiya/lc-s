d912fd531c9589e3bf62a12b325e56265676fa2e
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-03-11 14:21:39 -0400

    Time: 29 ms (7.97%) | Memory: 74.4 MB (9.61%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,24 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        if (preorder.empty() || inorder.empty()) return nullptr; 
-        
-        int rootVal = preorder[0];  
-        TreeNode* root = new TreeNode(rootVal);
-        
-        auto it = find(inorder.begin(), inorder.end(), rootVal);
-        int rootIndex = distance(inorder.begin(), it);
- 
-        vector<int> leftInorder(inorder.begin(), inorder.begin() + rootIndex);
-        vector<int> rightInorder(inorder.begin() + rootIndex + 1, inorder.end());
-        
-        vector<int> leftPreOrder(preorder.begin() + 1, preorder.begin() + 1 + rootIndex);
-        vector<int> rightPreOrder(preorder.begin() + 1 + rootIndex, preorder.end());
-        
+        if (preorder.empty() || inorder.empty()) {
+            return nullptr;
+        }
+
+        TreeNode* root = new TreeNode(preorder[0]);
+    
+        auto index = find(inorder.begin(), inorder.end(), preorder[0]) - inorder.begin();
+
+        vector<int> leftInorder (inorder.begin(), inorder.begin() + index);
+        vector<int> rightInorder (inorder.begin() + index + 1, inorder.end());
+        vector<int> leftPreOrder (preorder.begin()+1, preorder.begin() + index + 1);
+        vector<int> rightPreOrder (preorder.begin()+index+1, preorder.end());
 
         root->left = buildTree(leftPreOrder, leftInorder);
         root->right = buildTree(rightPreOrder, rightInorder);
-        
+
         return root;
+
+
     }
 };
