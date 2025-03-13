7a06f987573eca7bed92411a05229318de25e100
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-03-13 15:22:22 -0400

    Time: 2 ms (22.33%) | Memory: 16.4 MB (68.16%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,30 +1,19 @@
 class Solution {
+private:
+    TreeNode* helper(int& i, vector<int>& preorder, int ub) {  
+        if (i == preorder.size() || preorder[i] > ub) return nullptr;
+
+        TreeNode* root = new TreeNode(preorder[i++]);
+
+        root->left = helper(i, preorder, root->val);  
+        root->right = helper(i, preorder, ub);      
+
+        return root;
+    }
+
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        if (preorder.empty()) return nullptr; 
-
-        TreeNode* root = new TreeNode(preorder[0]); 
-        
-        for(int i = 1; i < preorder.size(); i++) {
-            TreeNode* dummy = root; 
-            TreeNode* newNode = new TreeNode(preorder[i]);
-
-            while (true) { 
-                if (preorder[i] < dummy->val) { 
-                    if (dummy->left == nullptr) { 
-                        dummy->left = newNode; 
-                        break;
-                    }
-                    dummy = dummy->left;
-                } else { 
-                    if (dummy->right == nullptr) { 
-                        dummy->right = newNode; 
-                        break;
-                    }
-                    dummy = dummy->right; 
-                }
-            }
-        }
-        return root;
+        int i = 0;
+        return helper(i, preorder, INT_MAX);
     }
 };
