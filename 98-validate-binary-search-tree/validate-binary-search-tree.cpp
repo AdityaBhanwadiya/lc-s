4fcca682f01510eabfc8181425cff99b9bc027e3
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-03-13 14:00:58 -0400

    Time: 0 ms (100.00%) | Memory: 21.9 MB (76.47%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,27 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-public:
-    bool helper(TreeNode* root, long long left, long long right) {
-        if(root == NULL)
-            return true;
+private:
+    bool helper(TreeNode* root, long long minVal, long long maxVal) {
+        if (root == NULL) return true;  
 
-        if(root->val <= left || root->val >= right) {
-            return false;
-        }
-        return helper(root->left, left, root->val) && helper(root->right, root->val, right);
+        if (root->val <= minVal || root->val >= maxVal) return false;
+
+        return helper(root->left, minVal, root->val) && 
+               helper(root->right, root->val, maxVal);
     }
+
+public:
     bool isValidBST(TreeNode* root) {
-        if(root == NULL)    return false;
         return helper(root, LLONG_MIN, LLONG_MAX);
     }
 };
