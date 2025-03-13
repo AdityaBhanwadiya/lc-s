36c7fe97c06b9ab7cb9541a12a76974850a7c470
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-03-13 11:07:02 -0400

    Time: 0 ms (100.00%) | Memory: 24.6 MB (18.23%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,17 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public:
-    void inorder(TreeNode* root, vector<int>& ans) {
-        if (root == NULL)
-            return;
-        
-        inorder(root->left, ans);  
-        ans.push_back(root->val);  
-        inorder(root->right, ans); 
+private:
+    void traverse (TreeNode* root, priority_queue<int, vector<int>, greater<int>>& minHeap) {
+        if(root == NULL)    return;
+        minHeap.push(root->val);
+        if(root->left)  traverse(root->left, minHeap);
+        if(root->right)  traverse(root->right, minHeap);
     }
-
+public:
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> answer;
-        inorder(root, answer);
-        return answer[k - 1];  
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        traverse(root, minHeap);
+
+        int ans = INT_MIN;
+        while(k-- > 0) {
+            ans = minHeap.top();
+            minHeap.pop();
+        }
+        return ans;
     }
 };
