0b0d24ac184982ad20120b4464097d7040e87ce8
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-03-18 16:13:26 -0400

    Time: 0 ms (100.00%) | Memory: 17.4 MB (39.39%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,26 +1,41 @@
 class Solution {
-public:
-    bool dfs(int start, int wantedColor, vector<int>& color, vector<vector<int>>& graph) {
-        color[start] = wantedColor;
+private: 
+    bool check (int start, vector<int> &color, vector<vector<int>>& graph) {
+        queue<int> q;
 
-        for(int neighbor : graph[start]) {
-            if(color[neighbor] == -1){
-                if(dfs(neighbor, !wantedColor, color, graph) == false)   return false;
-            }else if(color[neighbor] == color[start])
-                return false;
+        q.push(start);
+        color[start] = 0;
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for(int neighbor : graph[node]) {
+                if(color[neighbor] == -1) { 
+                    // Assign opposite color
+                    color[neighbor] = 1 - color[node];  
+                        q.push(neighbor);
+                } 
+                else if(color[neighbor] == color[node]) {  
+                    return false;
+                }
+            }
         }
         return true;
     }
+public:
     bool isBipartite(vector<vector<int>>& graph) {
+
         int n = graph.size();
+        int m = graph[0].size();
 
-        vector<int> color (n, -1);
+        // store coloring of nodes
+        vector<int> color(n, -1);
 
-        for(int i=0;i<n;i++) {
+        for(int i=0;i<n;i++){
             if(color[i] == -1){
-                if(dfs(i, 0, color, graph) == false){
+                if(check(i, color, graph) == false)
                     return false;
-                }
             }
         }
         return true;
