6d3c94d4f0bab616ef48fbab209057cd9bd0ad3b
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-03-18 14:04:39 -0400

    Time: 13 ms (30.20%) | Memory: 38.8 MB (19.55%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,48 +1,45 @@
 class Solution {
+private:
+    void dfs(int i, int j, int n, int m, vector<vector<int>>& grid, vector<vector<int>>& vis, vector<int>& dx, vector<int>& dy) {
+        vis[i][j] = 1;
+
+        for(int k=0;k<4;k++){
+            int nx = i + dx[k];
+            int ny = j + dy[k];
+
+            if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 1 && vis[nx][ny] == 0){
+                vis[nx][ny]=1;
+                dfs(nx, ny, n, m, grid, vis, dx, dy);
+            }
+        }
+    }
 public:
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> vis (n, vector<int> (m, 0));
 
-        queue<pair<int, int>> q;
-        
-        vector<int> dirx = {-1, 1, 0, 0};
-        vector<int> diry = {0, 0, -1, 1};
+        vector<int> dx = {-1, 1, 0, 0};
+        vector<int> dy = {0, 0, -1, 1};
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i == 0 || i == n-1 || j == 0 || j == m-1) {
-                    if(grid[i][j] == 1){
-                        q.push({i, j});
-                        vis[i][j] = 1;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(i == 0 || i == n-1 || j == 0 || j == m-1){
+                    if(grid[i][j] == 1 && vis[i][j] == 0) {
+                        dfs(i, j, n, m, grid, vis, dx, dy);
                     }
                 }
             }
         }
 
-        while(!q.empty()) {
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-
-            for(int k = 0;k<4;k++){
-                int nx = x + dirx[k];
-                int ny = y + diry[k];
-
-                if(nx >= 0 && nx < n && ny>=0 && ny < m && grid[nx][ny] == 1 && vis[nx][ny] == 0) {
-                    q.push({nx, ny});
-                    vis[nx][ny] = 1;
-                }
-            }
-        }
-
         int count = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 1 && vis[i][j] == 0)
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++) {
+                if(grid[i][j] == 1 && vis[i][j] == 0){
                     count++;
+                }
             }
         }
         return count;
