0353cceced8a300956b73d01dcf73bae1fdff4c9
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-03-28 11:43:35 -0500

    Time: 15 ms (69.24%) | Memory: 46.6 MB (58.51%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,50 +1,31 @@
-class DisjointSets {
-
+class Disjointset {
 public:
-    vector<int> rank, size, parent;
-    DisjointSets(int n) {
-        rank.resize(n+1, 0);
-        size.resize(n+1, 1);
+    vector<int> rank, parent;
+    Disjointset(int n){
         parent.resize(n+1);
+        rank.resize(n+1, 0);
 
-        for(int i=1;i<=n;i++) {
-            parent[i] = i;
-        }
+        for(int i=0;i<=n;i++)
+            parent[i]=i;
     }
-
     int findParent(int node) {
-        if(node == parent[node])
+        if(parent[node] == node)
             return node;
         return parent[node] = findParent(parent[node]);
     }
 
-    void findUnionByRank (int u, int v) {
-        int ul_pu = findParent(u);
-        int ul_pv = findParent(v);
+    void unionByRank (int u, int v) {
+        int ulpu = findParent(u);
+        int ulpv = findParent(v);
 
-        if(ul_pu == ul_pv)  return;
-
-        if(rank[ul_pu] < rank[ul_pv]) {
-            parent[ul_pu] = ul_pv;
-        }else if(rank[ul_pv] < rank[ul_pu]){
-            parent[ul_pv] = ul_pu;
+        if(ulpu == ulpv)    return;
+        if(rank[ulpu] == rank[ulpv]){
+            parent[ulpu] = ulpv;
+            rank[ulpv]++;
+        }else if(rank[ulpu] < rank[ulpv]){
+            parent[ulpu] = ulpv;
         }else{
-            parent[ul_pv] = ul_pu;
-            rank[ul_pu]++;
-        }
-    }
-
-    void findUnionBySize(int u, int v) {
-        int ul_pu = findParent(u);
-        int ul_pv = findParent(v);
-
-        if(ul_pu == ul_pv)  return;
-        if(size[ul_pu] < size[ul_pv]) {
-            parent[ul_pu] = ul_pv;
-            size[ul_pv] += size[ul_pu];
-        }else{
-            parent[ul_pv] = ul_pu;
-            size[ul_pu] += size[ul_pv];
+            parent[ulpv] = ulpu;
         }
     }
 };
@@ -52,24 +33,20 @@ public:
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        DisjointSets ds(n);
-        int cntExtras = 0;
-        for (auto it : connections) {
-            int u = it[0];
-            int v = it[1];
-            if (ds.findParent(u) == ds.findParent(v)) {
-                cntExtras++;
-            }
-            else {
-                ds.findUnionBySize(u, v);
+        if(connections.size() < n-1)    return -1;
+        Disjointset ds(n);
+
+        for(auto it : connections) {
+            if(ds.findParent(it[0]) != ds.findParent(it[1])){
+                ds.unionByRank(it[0], it[1]);
             }
         }
-        int cntC = 0;
-        for (int i = 0; i < n; i++) {
-            if (ds.parent[i] == i) cntC++;
+
+        int components = 0;
+        for(int i=0;i<n;i++){
+            if(ds.parent[i] == i)
+                components++;
         }
-        int ans = cntC - 1;
-        if (cntExtras >= ans) return ans;
-        return -1;
+        return components-1;
     }
 };
