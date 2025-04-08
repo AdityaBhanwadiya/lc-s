b243347e7e4ce5466c2cadf0c458c4f95d781550
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-04-08 15:03:43 -0400

    Time: 5 ms (83.06%) | Memory: 26.2 MB (49.65%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,27 +1,33 @@
 class Solution {
 private:
-    bool canDivide(const vector<int>& nums, int divisor, int threshold) {
-        int total = 0;
-        for (int num : nums) {
-            total += (num + divisor - 1) / divisor; 
+    bool possible (vector<int>& nums, int threshold, int divisor) {
+        int newSum = 0;
+        for(int i=0;i<nums.size();i++) {
+            newSum += (nums[i] + divisor - 1) / divisor;
         }
-        return total <= threshold;
+
+        return newSum<=threshold;
     }
 public:
+    // 1, 2, 5, 9
+    // sum = 17
+    // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
+    // l           m           r
+    // 
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int left = 1;
-        int right = *max_element(nums.begin(), nums.end());
-        int answer = right;
+        int left = 1, right = *max_element(nums.begin(), nums.end());
+        int result = -1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canDivide(nums, mid, threshold)) {
-                answer = mid;   
-                right = mid - 1;
-            } else {
-                left = mid + 1; 
+        while(left<= right) {
+            int divisor = left + (right - left) /2;
+
+            if(possible(nums, threshold, divisor)){
+                result = divisor;
+                right = divisor - 1;
+            }else{
+                left = divisor + 1;
             }
         }
-        return answer;
+        return result;
     }
 };
