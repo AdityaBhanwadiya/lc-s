44de3d8c837dd49cbea57d1efbe93e7c26402dd5
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-05-16 22:16:14 -0500

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.50%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& arr) {
-        int n = arr.size();
+        // int n = arr.size();
 
-        if(n == 1)  return arr[0];
-        int left = 0, right = n - 1;
+        // if(n == 1)  return arr[0];
+        // int left = 0, right = n - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        // while (left <= right) {
+        //     int mid = left + (right - left) / 2;
 
-            if (mid % 2 == 0) {
-                if (arr[mid] == arr[mid + 1]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            } else {
-                if (arr[mid] == arr[mid - 1]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
+        //     if (mid % 2 == 0) {
+        //         if (arr[mid] == arr[mid + 1]) {
+        //             left = mid + 1;
+        //         } else {
+        //             right = mid - 1;
+        //         }
+        //     } else {
+        //         if (arr[mid] == arr[mid - 1]) {
+        //             left = mid + 1;
+        //         } else {
+        //             right = mid - 1;
+        //         }
+        //     }
+        // }
+
+        // return arr[left];
+
+        int result = 0;
+        for(auto it: arr){
+            result ^= it;
         }
-
-        return arr[left];
+        return result;
     }
 };
