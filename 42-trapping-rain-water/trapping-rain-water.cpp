d835eabd26a9ea20923c9c763d940c6518fb74c5
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-05-19 11:11:43 -0500

    Time: 0 ms (100.00%) | Memory: 26 MB (60.25%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,27 +1,64 @@
+//Brute Force
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int n = height.size();
+//         int totalWater = 0;
+
+//         for (int i = 0; i < n; ++i) {
+//             int maxLeft = 0, maxRight = 0;
+
+//             // Scan left
+//             for (int j = i; j >= 0; --j) {
+//                 maxLeft = max(maxLeft, height[j]);
+//             }
+
+//             // Scan right
+//             for (int j = i; j < n; ++j) {
+//                 maxRight = max(maxRight, height[j]);
+//             }
+
+//             // Water level is min of both sides
+//             int waterLevel = min(maxLeft, maxRight);
+
+//             // Water trapped = water level - current height
+//             totalWater += waterLevel - height[i];
+//         }
+
+//         return totalWater;
+//     }
+// };
+
+
+// Optimal Two pointer Approach
 class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        if(n == 0) return 0;
+        if (n == 0) return 0;
 
-        vector<int> leftMax(n), rightMax(n);
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
         int water = 0;
 
-        // Build leftMax array
-        leftMax[0] = height[0];
-        for(int i = 1; i < n; i++) {
-            leftMax[i] = max(leftMax[i - 1], height[i]);
-        }
-
-        // Build rightMax array
-        rightMax[n - 1] = height[n - 1];
-        for(int i = n - 2; i >= 0; i--) {
-            rightMax[i] = max(rightMax[i + 1], height[i]);
-        }
-
-        // Calculate trapped water
-        for(int i = 0; i < n; i++) {
-            water += min(leftMax[i], rightMax[i]) - height[i];
+        while (left < right) {
+            if (height[left] < height[right]) {
+                // Left is the limiting side
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];  // update max
+                } else {
+                    water += leftMax - height[left];  // trap water
+                }
+                left++;
+            } else {
+                // Right is the limiting side
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+                right--;
+            }
         }
 
         return water;
