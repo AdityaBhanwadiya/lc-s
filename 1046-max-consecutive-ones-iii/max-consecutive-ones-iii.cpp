01b66ebaa6cd19b6695c94d07eff16db1f2c4e50
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-05-19 13:27:22 -0500

    Time: 3 ms (28.27%) | Memory: 59.3 MB (58.47%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -10,7 +10,7 @@ public:
         while(r < n) {
             if(nums[r] == 0)    zeros++;
 
-            while(zeros > k){
+            if(zeros > k){
                 if(nums[l] == 0) zeros--;
                 l++;
             }
