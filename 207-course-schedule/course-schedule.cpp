8e079d0051315815c272511880514343cb5c645b
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-05-20 08:45:42 -0500

    Time: 0 ms (100.00%) | Memory: 19.1 MB (94.58%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,35 +1,48 @@
 class Solution {
-private:
-    bool checkCycles (int start, vector<int>& vis, vector<int>& path, vector<vector<int>>& prereq)     {   
-        vis[start] = 1;
-        path[start] = 1;
-
-        for (int neighbor : prereq[start]) {
-            if (!vis[neighbor]) {  
-                if (checkCycles(neighbor, vis, path, prereq)) 
-                    return true; 
-            } 
-            else if (path[neighbor]) {
-                return true; 
-            }
-        }
-        path[start] = 0;
-        return false;
-    }
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> visited(numCourses, 0);
-        vector<int> path (numCourses, 0);
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
+        // [1, 0] means to take 1, you gtta take 0 first
+        // Take v before u
+        // SOmething before soemthing -> Topo Sort
+        // Kahns algo -> indegree vector, queue, adjList
 
         vector<vector<int>> adjList(numCourses);
-        for (auto &p : prerequisites) {
+        for (auto &p : pre) {
             adjList[p[1]].push_back(p[0]);
         }
-        
+
+
+        vector<int> indegree (numCourses, 0);
         for(int i=0;i<numCourses;i++){
-            // If cycle found
-            if(!visited[i] && checkCycles(i, visited, path, adjList)) 
-                return false;
+            for(auto it : adjList[i]) {
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+
+        while(!q.empty()) {
+            auto node = q.front();
+            q.pop();
+
+            for(auto it : adjList[node]) {
+                indegree[it]--;
+
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+
+        for(auto it:indegree){
+            if(it != 0) return false;
         }
         return true;
     }
