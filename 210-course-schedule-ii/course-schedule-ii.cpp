dd2c31c67ca6f3e602238b5877f409e19d9b447d
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-05-20 09:15:07 -0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (17.02%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,37 +1,42 @@
 class Solution {
+private:
+    bool dfs(int i, stack<int>& st, vector<int>& vis, vector<int>& rec, vector<vector<int>>& adjList) {
+        vis[i] = 1;
+        rec[i] = 1;
+
+        for (auto it : adjList[i]) {
+            if (!vis[it]) {
+                if (!dfs(it, st, vis, rec, adjList)) return false;
+            } else if (rec[it]) {
+                return false; 
+            }
+        }
+
+        rec[i] = 0;
+        st.push(i);
+        return true;
+    }
+
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-
-        vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses, 0);
-        vector<int> topo;
-        queue<int> q;
-
-        for (auto& pre : prerequisites) {
-            int course = pre[0], prereq = pre[1];
-            adj[prereq].push_back(course);
-            indegree[course]++;
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<vector<int>> adjList(n);
+        for (auto it : pre) {
+            adjList[it[1]].push_back(it[0]);
         }
 
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
+        stack<int> st;
+        vector<int> vis(n, 0), rec(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                if (!dfs(i, st, vis, rec, adjList)) return {}; 
             }
         }
 
-
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-
-            for(auto it : adj[node]){
-                indegree[it]--;
-                if(indegree[it] == 0){
-                    q.push(it);
-                }
-            }
+        vector<int> result;
+        while (!st.empty()) {
+            result.push_back(st.top());
+            st.pop();
         }
-        return topo.size() == numCourses ? topo : vector<int>();
+        return result;
     }
 };
