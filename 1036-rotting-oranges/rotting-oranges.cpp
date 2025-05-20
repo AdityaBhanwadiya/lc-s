e85b62a99dce5413e347265f6745ed878ec06e36
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-05-20 07:43:59 -0500

    Time: 0 ms (100.00%) | Memory: 16.5 MB (96.99%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -4,47 +4,48 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
+        // classic bfs problem
+        // keep coutn of fresh oranges and store rotten in queue
+
         queue<pair<int, int>> q;
         int fresh = 0;
 
         for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 2){
+            for(int j=0;j<m;j++) {
+                if(grid[i][j] == 1) fresh++;
+                else if(grid[i][j] == 2)    {
                     q.push({i, j});
-                }else if(grid[i][j] == 1){
-                    fresh++;
                 }
             }
         }
 
-        vector<int> dirx = {-1, 1, 0, 0};
-        vector<int> diry = {0, 0, -1, 1};
+        vector<int> dx = {-1, 1, 0, 0};
+        vector<int> dy = {0, 0, -1, 1};
 
-        int mins = 0;
+        int minutes = 0;
 
         while(!q.empty()) {
             int size = q.size();
 
             for(int i=0;i<size;i++) {
-                auto item = q.front();
+                auto [x, y] = q.front();
                 q.pop();
-                
-                for(int k=0;k<4;k++) {
-                    int nx = dirx[k] + item.first;
-                    int ny = diry[k] + item.second;
 
-                    if(nx>=0 && nx<n && ny>=0 && ny<m && grid[nx][ny] == 1) {
+                for(int k =0;k<4;k++) {
+                    int nx = x + dx[k];
+                    int ny = y + dy[k];
+
+                    if(nx<n && nx >= 0 && ny<m && ny>=0 && grid[nx][ny] == 1){
                         fresh--;
                         grid[nx][ny] = 2;
                         q.push({nx, ny});
                     }
                 }
-            }
-            mins++;
+            }  
+            minutes++;
         }
-        // last round might be off with no fresh oranges
-        if (mins > 0) mins--;
-        if(fresh == 0)  return mins;
+        if(minutes> 0)  minutes--;
+        if(fresh == 0)  return minutes;
         return -1;
     }
 };
