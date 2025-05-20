4d7a40f6005190ea84dee66e7e9602dc8583ba0a
Aditya Bhanwadiya <62671628+AdityaBhanwadiya@users.noreply.github.com>
2025-05-20 08:02:37 -0500

    Time: 30 ms (26.45%) | Memory: 38.8 MB (45.55%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,42 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+
         int n = mat.size();
         int m = mat[0].size();
 
-        vector<vector<int>> result (n, vector<int>(m, 0));
-        vector<vector<int>> visited(n, vector<int>(m, 0));
 
-        queue<pair<pair<int, int>, int>> q;
+        // you gotta use bfs and store the dist as well in q
+        queue<pair<int, pair<int, int>>> q;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        // create seperate result
+        vector<vector<int>> result (n, vector<int> (m, 0));
+
+        // visited as well
+        vector<vector<int>> visited (n, vector<int> (m, 0));
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
                 if(mat[i][j] == 0){
                     visited[i][j] = 1;
-                    q.push({{i, j}, 0});
+                    q.push({0, {i, j}});
                 }
             }
         }
 
-        vector<int> dirx = {-1, 1, 0, 0};
-        vector<int> diry = {0, 0, -1, 1};
+        vector<int> dx = {-1, 1, 0, 0};
+        vector<int> dy = {0, 0, -1, 1};
 
         while(!q.empty()) {
-            auto it = q.front();
-            int x = it.first.first;
-            int y = it.first.second;
-            int steps = it.second;
-            q.pop(); 
+            auto item = q.front();
+            int dist = item.first;
+            int x = item.second.first;
+            int y = item.second.second;
 
-            result[x][y] = steps;
+            q.pop();
 
             for(int k=0;k<4;k++) {
-                int nx = x + dirx[k];
-                int ny = y + diry[k];
+                int nx = x + dx[k];
+                int ny = y + dy[k];
 
-                if(nx>=0 && nx<n && ny>=0 && ny<m && visited[nx][ny] == 0){
+                if(nx<n && nx>=0 && ny<m && ny>=0 && !visited[nx][ny]) {
                     visited[nx][ny] = 1;
-                    q.push({{nx, ny}, steps+1});
+                    result[nx][ny] = dist+1;
+                    q.push({result[nx][ny], {nx, ny}});
                 }
             }
         }
